2d841c56c4f57372d8b54a333c6b3ec04bc20d8c
Yuval FBP <yuvalfbp@gmail.com>
2024-09-22 18:56:51 +0300

    Added structure for WristIO

---
+++ src/main/java/frc/robot/subsystems/wrist/io/WristIO.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.wrist.io;
+
+import java.util.function.DoubleSupplier;
+
+import frc.lib.logfields.IOBase;
+import frc.lib.logfields.LogFieldsTable;
+
+/** Add your docs here. */
+public abstract class WristIO extends IOBase {
+
+    public final DoubleSupplier wristAngleDegrees = fields.addDouble("wristAngleDegrees", this::getWristAngleDegrees);
+
+    protected WristIO(LogFieldsTable fieldsTable) {
+        super(fieldsTable);
+    }
+
+    // Inputs
+    protected abstract double getWristAngleDegrees();
+
+    // Outputs
+    protected abstract void setSpeed(double wristSpeed);
+
+}
---
+++ src/main/java/frc/robot/subsystems/wrist/io/WristIOSparkMax.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.wrist.io;
+
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import frc.lib.logfields.LogFieldsTable;
+
+/** Add your docs here. */
+public class WristIOSparkMax extends WristIO {
+    private final Spark
+    protected WristIOSparkMax(LogFieldsTable fieldsTable) {
+        super(fieldsTable);
+    }
+
+}
