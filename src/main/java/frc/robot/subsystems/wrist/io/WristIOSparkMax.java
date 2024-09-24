beb431fe025b63fa4623f344ba64fa10d6029098
Eyal Eldan-Miller <eyal.el2021@leyada.net>
2024-09-24 16:52:52 +0300

    Saved changes to IOSparkmax

--- src/main/java/frc/robot/subsystems/wrist/io/WristIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/wrist/io/WristIOSparkMax.java
@@ -4,14 +4,32 @@
 
 package frc.robot.subsystems.wrist.io;
 
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import frc.lib.logfields.LogFieldsTable;
 
 /** Add your docs here. */
 public class WristIOSparkMax extends WristIO {
-    private final SparkMa
+    private final CANSparkMax wristMotorLeft = new CANSparkMax(0, MotorType.kBrushless);
+    private final CANSparkMax wristMotorRight = new CANSparkMax(0, MotorType.kBrushless);
+    private final DutyCycleEncoder wristEncoder = new DutyCycleEncoder(0);
     protected WristIOSparkMax(LogFieldsTable fieldsTable) {
         super(fieldsTable);
+        wristMotorRight.follow(wristMotorLeft);
+        wristMotorLeft.setSmartCurrentLimit(25);
+        wristMotorLeft.setIdleMode(IdleMode.kBrake);
+    }
+
+    @Override
+    public double getWristAngleDegrees() {
+        return(wristEncoder.getAbsolutePosition()) * 360;
+    }
+    @Override
+    public void setSpeed(double speed) {
+        wristMotorLeft.set(speed);
     }
 
 }
