68d75ed6be90e7ed29d43f1ed749c7ecc1dd8603
Atlantis#2679 <atlantis2679@gmail.com>
2024-10-06 15:57:02 -0700

    working on Iosim

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -4,13 +4,20 @@
 
 package frc.robot.subsystems.shooter;
 
+import edu.wpi.first.math.system.LinearSystem;
+
 /** Add your docs here. */
 public final class ShooterConstants {
     public static final int MAX_VOLTAGE = 0;
 
     public static final int ROLLER_CURRENT_LIMIT = 30;
+
     public static final int kP = 1;
     public static final int kD = 0;
     public static final int kI = 0;
+
+    // sim
+    public static final LinearSystem LINEAR_SYSTEM = new LinearSystem<>(null, null, null, null);
+    public static final double GERAING = 0;
 
 }
---
+++ src/main/java/frc/robot/subsystems/shooter/io/ShooterIOSim.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems.shooter.io;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+
+import frc.lib.logfields.LogFieldsTable;
+import frc.robot.RobotMap.CANBUS;
+import frc.robot.subsystems.shooter.ShooterConstants;
+
+public class ShooterIOSim extends ShooterIO {
+    private final FlywheelSim upperRollorSim = new FlywheelSim(ShooterConstants.LINEAR_SYSTEM, DCMotor.getNEO(1),
+            ShooterConstants.GERAING);
+    private final FlywheelSim lowerRollorSim = new FlywheelSim(ShooterConstants.LINEAR_SYSTEM, DCMotor.getNEO(1),
+            ShooterConstants.GERAING);
+
+    public ShooterIOSim(LogFieldsTable fieldsTable) {
+        super(fieldsTable);
+    }
+
+    @Override
+    protected double getUpperRollerSpeedRPM() {
+        return upperRollorSim.getAngularVelocityRPM();
+    }
+
+    @Override
+    protected double getLowerRollerSpeedRPM() {
+        return lowerRollorSim.getAngularVelocityRPM();
+    }
+
+    @Override
+    public void setUpperRollerVoltage(double voltage) {
+        upperRollorSim.setInputVoltage(voltage);
+    }
+
+    @Override
+    public void setLowerRollerVoltage(double voltage) {
+        upperRollorSim.setInputVoltage(voltage);
+    }
+
+}
