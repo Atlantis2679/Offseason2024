1217019c5b2225aca96d8c5517d5e886fea0099c
Eyal Eldan-Miller <eyal.el2021@leyada.net>
2024-10-08 18:11:45 +0300

    added swerve subsystem (from season code)

---
+++ src/main/java/frc/robot/subsystems/swerve/io/GyroIONavX.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems.swerve.io;
+
+import com.kauailabs.navx.frc.AHRS;
+import frc.lib.logfields.LogFieldsTable;
+
+import static frc.robot.RobotMap.*;
+
+
+public class GyroIONavX extends GyroIO{
+    private final AHRS navX = new AHRS(NAVX_PORT);
+    
+    public GyroIONavX(LogFieldsTable fieldsTable) {
+        super(fieldsTable);
+    }
+
+    @Override 
+    protected double getYawDegreesCW() {
+        return navX.getYaw();
+    }
+
+    @Override
+    protected boolean isConnected() {
+        return navX.isConnected();
+    }
+
+    @Override
+    protected boolean isMoving(){
+        return navX.isMoving();
+    }
+}
