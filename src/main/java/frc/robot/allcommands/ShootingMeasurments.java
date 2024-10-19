521d12a477af624542613f03b794c10cc4e5fa74
Roi Bar <roi3bar@gmail.com>
2024-10-19 23:34:23 +0300

    fixed again to real measure

--- src/main/java/frc/robot/allcommands/ShootingMeasurments.java
+++ src/main/java/frc/robot/allcommands/ShootingMeasurments.java
@@ -2,9 +2,9 @@ package frc.robot.allcommands;
 
 public class ShootingMeasurments {
     public static ShootingState[] ALL_MEASURMENTS = new ShootingState[] {
-        new ShootingState(1.1, -24, 2000, 2000),
-        new ShootingState(2.0, -10, 2000, 2000),
-        new ShootingState(2.76, -4.5, 2000, 2000),
-        new ShootingState(3.4, 3, 3000, 3000),
+        new ShootingState(1.3, -24, 2000, 2000),
+        new ShootingState(2.19, -10, 2000, 2000),
+        new ShootingState(2.96, -4.5, 2000, 2000),
+        new ShootingState(3.6, 3, 3000, 3000),
     };
 }
