6ca77dbe002fce5a1e70ee48f7546e35995bba41
Yair Bligh <yairbligh@gmail.com>
2024-11-05 16:38:31 +0200

    change subwoofer and collect  pivot angle to -27 and changed amp pivot angle to 59

--- src/main/java/frc/robot/allcommands/AllCommandsConstants.java
+++ src/main/java/frc/robot/allcommands/AllCommandsConstants.java
@@ -1,13 +1,13 @@
 package frc.robot.allcommands;
 
 public class AllCommandsConstants {
-    public static final double PIVOT_ANGLE_FOR_INTAKE = -25;
+    public static final double PIVOT_ANGLE_FOR_INTAKE = -27;// was -25
 
     public static final double DELIVERY_PIVOT_ANGLE = 10;
     public static final double DELIVERY_LAUNCHER_SPEED = -1;
 
     public static final double LAUNCHER_SHOOT_SPEED = 1;
-    
+
     public static final double LAUNCHER_COLLECT_SPEED = 0.5;
 
     public static class ManualController {
@@ -18,11 +18,11 @@ public class AllCommandsConstants {
     }
 
     public static class GetReadyToShoot {
-        public static final double SUBWOOFER_PIVOT_ANGLE = -24;
+        public static final double SUBWOOFER_PIVOT_ANGLE = -27;// was -24
         public static final double SUBWOOFER_UPPER_ROLLER_SPEED = 2000;
         public static final double SUBWOOFER_LOWER_ROLLER_SPEED = 2000;
 
-        public static final double AMP_PIVOT_ANGLE = 62;
+        public static final double AMP_PIVOT_ANGLE = 59;// was 62
         public static final double AMP_UPPER_ROLLER_SPEED = 1200;
         public static final double AMP_LOWER_ROLLER_SPEED = 500;
 
